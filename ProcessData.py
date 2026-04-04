b659679f759a82cee9a982886689625d0e4c6beb
joeygargar <miagarcia@unomaha.edu>
2026-04-04 21:13:25 +0000

    lab8

--- ProcessData.py
+++ ProcessData.py
@@ -1,21 +1,49 @@
 #ProcessData.py
-#Name:
+#Name: Mia Garcia
 #Date:
-#Assignment:
+#Assignment: Lab 8 
 
 import random
 
 def main():
 
-  #Open the files we will be using
   inFile = open("names.dat", 'r')
   outFile = open("StudentList.csv", 'w')
 
-  #Process each line of the input file and output to the CSV file
+  for line in inFile:
+    info = line.strip().split(" ")
 
+    first = info[0]
+    last = info[1]
+    studentID = info[3]
+    year = info[5]
+    major = info[6]
 
+    firstInitial = first[0].lower()
+    lastLower = last.lower()
 
-  #Close files in the end to save and ensure they are not damaged.
+    lastThree = studentID[-3:]
+
+    if len(lastLower) < 5:
+      lastLower += "Y"
+
+    userID = firstInitial + lastLower + lastThree
+
+    majorCode = major[:3].upper()
+
+    if year == "Freshman":
+      yearCode = "FR"
+    elif year == "Sophomore":
+      yearCode = "SO"
+    elif year == "Junior":
+      yearCode = "JR"
+    elif year == "Senior":
+      yearCode = "SR"
+
+    majorYear = majorCode + "-" + yearCode
+
+    outFile.write(last + "," + first + "," + userID + "," + majorYear + "\n")
+
   inFile.close()
   outFile.close()
 
